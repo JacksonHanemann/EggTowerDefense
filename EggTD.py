ffe1bbaab214f73b3bbcaac22a3efc9cb4f849ae
christian-jayride <95272082+christian-jayride@users.noreply.github.com>
2023-05-17 21:13:34 +1000

    Added rounds, fixed buttons

--- EggTD.py
+++ EggTD.py
@@ -8,16 +8,16 @@ import os
 from pygame import mixer
 
 
-#written by Jackson Hanemann
+# Written by Jackson Hanemann
 
-#Screen dimensions in pixels
+# Screen dimensions in pixels
 WIDTH = 1920
 HEIGHT = 1080
 
-#Frames per second
+# Frames per second
 FPS = 60
 
-#initalise pygame
+# Initalise pygame
 pygame.init()
 GameDisplay = pygame.display.set_mode((WIDTH,HEIGHT))
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -98,13 +98,17 @@ def game():
     cash = 100
     cashIncrement = 15
     weaponCost = 50
+    upgradeCost = 50
 
-    #health system+
+    #health system
     health = 100
     healthDecrease = -1
 
     #Buy button text
-    buyBut = smallFont.render('New Cannon', True, WHITE)
+    buyBut = smallFont.render('NEW CANNON', True, WHITE)
+
+    #upgrade button text
+    upgBut =  smallFont.render('UPGRADE CANNON', True, WHITE)
 
 
     #BACKGROUND
@@ -116,31 +120,20 @@ def game():
 
     # Set up rounds 
     roundOne = {'eggCount': 5, 'eggSpeed': 5}
-    #roundTwo = {'eggCount': 7, 'eggSpeed': 6}
-    #roundThree = {'eggCount': 10, 'eggSpeed': 7}
-    #allRounds = [roundOne, roundTwo, roundThree]
-    allRounds = [roundOne]
-
+    roundTwo = {'eggCount': 7, 'eggSpeed': 6}
+    roundThree = {'eggCount': 10, 'eggSpeed': 7}
+    allRounds = [roundOne, roundTwo, roundThree]
+    
     allEggs = []
 
-    for nextRound in allRounds:
-        numEggs = nextRound['eggCount']
-        speed = nextRound['eggSpeed']
-
-        for eachEgg in range(numEggs):
-            #print("Creating egg")
-            newEgg = Egg(WIDTH, HEIGHT, speed)
-            allEggs.append(newEgg)
-
     # Create all eggs
-    '''Egg1 = Egg(WIDTH, HEIGHT)
-    Egg2 = Egg(WIDTH, HEIGHT)
-    Egg3 = Egg(WIDTH, HEIGHT)
-    Egg4 = Egg(WIDTH, HEIGHT)
-    Egg5 = Egg(WIDTH, HEIGHT)'''
+    #for nextRound in allRounds:
+    #    numEggs = nextRound['eggCount']
+    #    speed = nextRound['eggSpeed']
 
-    # Egg array
-    # allEggs = [Egg1, Egg2, Egg3, Egg4, Egg5]
+    #   for eachEgg in range(numEggs):
+    #        newEgg = Egg(WIDTH, HEIGHT, speed)
+    #       allEggs.append(newEgg)
 
     myCannon1 = Cannon(WIDTH/2, HEIGHT-900, 'down', 5)
     myCannon2 = Cannon(WIDTH-400, HEIGHT-320, 'up', 5)
@@ -150,106 +143,178 @@ def game():
     spawnFirstEgg = True
     activateFirstWeapon = True
     newWeapon = False
+    hasUpgraded = False
 
-    click = False
+    clickBuy = False
+    clickUpg = False
+    cantUpg = False
 
-    while len(allEggs) > 0:
-        #print(len(allEggs))
 
-        # Draw the background and score to the screen       
-        screen.fill(BLACK)
-        screen.blit(Map, MapRect)
-        cash_text = font.render(f'Cash: ${cash}', True, (0, 0, 0))
-        screen.blit(cash_text, (1500, 150))
-        health_text = font.render(f'Health: {health}', True, (0, 0, 0))
-        screen.blit(health_text, (1500, 200))
-
-        mx, my = pygame.mouse.get_pos()
-        button3 = pygame.Rect(860,240,50,50)
-
-        if button3.collidepoint(mx, my):
-            if click:
-                #print("working")
-                newWeapon = True
-                click = False
+    for nextRound in allRounds:
+        print("This is round %s" % allRounds.index(nextRound))
         
-        pygame.draw.rect(screen, (255, 0, 0), button3)
-        pygame.draw.rect(screen, BLACK, [WIDTH/1.25, HEIGHT/3.5, 140, 40])
-        screen.blit(buyBut, (WIDTH/1.25+10, HEIGHT/3.5+10,))
+        #Create eggs for next round
+        numEggs = nextRound['eggCount']
+        speed = nextRound['eggSpeed']
 
-        for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    click = True
+        for eachEgg in range(numEggs):
+            newEgg = Egg(WIDTH, HEIGHT, speed)
+            allEggs.append(newEgg)
 
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    main_menu()
+    #while health > 0:
+        
+        
+        while len(allEggs) > 0 and health > 0:
+            mixer.init()
+            mixer.music.load('LobbyMusic.mp3')
+            mixer.music.stop
 
-        for myEgg in allEggs:
-            #print("Egg is alive: " + str(allEggs.index(myEgg)))
-            #print(type(myEgg))
-            # Total time elapsed since the timer started
-            totaltime = round((time.time() - starttime), 2)
+            # Draw the background and score to the screen       
+            screen.fill(BLACK)
+            screen.blit(Map, MapRect)
+            cash_text = font.render(f'Cash: ${cash}', True, (0, 0, 0))
+            screen.blit(cash_text, (1500, 150))
+            health_text = font.render(f'Health: {health}', True, (0, 0, 0))
+            screen.blit(health_text, (1500, 200))
 
-            # Check if we need to spawn an egg
-            if (totaltime > 3 and not myEgg.isAlive) or spawnFirstEgg:
-                #print("Egg is alive: " + str(allEggs.index(myEgg)))
-                myEgg.isAlive = True
-                spawnFirstEgg = False
-                starttime = time.time()
+            mx, my = pygame.mouse.get_pos()
+            button3 = pygame.Rect(WIDTH/1.25, HEIGHT/3.5, 160, 40)
+            button4 = pygame.Rect(WIDTH/1.25, HEIGHT/2.5-50, 210, 40)
 
-            if not myEgg.isAlive:
-                continue
+            if button3.collidepoint(mx, my):
+                if click:
+                    if cash >= weaponCost:
+                        newWeapon = True
+                        cash -= weaponCost             
+            
+            if button4.collidepoint(mx, my):
+                if click:
+                    if cash >= upgradeCost:
+                        hasUpgraded = True
+                        cash -= upgradeCost
+            
+            click = False
 
-            # If egg is alive, move it along the path
-            myEgg.move(5, TurningP)
-            myEgg.setImage()
-            screen.blit(myEgg.eggImage, myEgg.eggRect)
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        main_menu()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        click = True
 
-            # If egg is safely home, remove it from the list of active eggs and substract from player health
-            if myEgg.eggRect.x > 1928:
-                health += healthDecrease
-                allEggs.remove(myEgg)
-                continue
+            pygame.draw.rect(screen, BLACK, [WIDTH/1.25, HEIGHT/3.5, 160, 40])
+            screen.blit(buyBut, (WIDTH/1.25+10, HEIGHT/3.5+10,))
 
-        # Check all weeapons
-        for weapon in allWeapons:
-            if (activateFirstWeapon or newWeapon) and not weapon.isActive:
-                #print("Activating weapon")
-                weapon.isActive = True
-                activateFirstWeapon = False
+            pygame.draw.rect(screen, BLACK, [WIDTH/1.25, HEIGHT/2.5-50, 210, 40])
+            screen.blit(upgBut, (WIDTH/1.25+10, HEIGHT/2.5-40,))
 
-                # If this weapon was bought, subtract from cash
-                if newWeapon:
-                    cash -= weaponCost
-                    newWeapon = False
+            for myEgg in allEggs:
+                # Total time elapsed since the timer started
+                totaltime = round((time.time() - starttime), 2)
 
-            if not weapon.isActive:
-                continue
+                # Check if we need to spawn an egg
+                if (totaltime > 3 and not myEgg.isAlive) or spawnFirstEgg:
+                    myEgg.isAlive = True
+                    spawnFirstEgg = False
+                    starttime = time.time()
 
-            # Move ammo
-            weapon.moveAmmo()
+                if not myEgg.isAlive:
+                    continue
 
-            # Check if ammo hit an egg
-            eggHit = weapon.checkHit(allEggs)
+                # If egg is alive, move it along the path
+                myEgg.move(5, TurningP)
+                myEgg.setImage()
+                screen.blit(myEgg.eggImage, myEgg.eggRect)
 
-            if eggHit == -1:
-                screen.blit(weapon.ammoImage, weapon.ammoRect)
-            else:
-                #print("Blit boom")
-                weapon.boomRect.center = allEggs[eggHit].eggRect.center
-                screen.blit(weapon.boomImage, weapon.boomRect)
-                cash += cashIncrement
+                # Is egg safely home?
+                if myEgg.eggRect.x > 1928:
+                    # Subtract from health
+                    health += healthDecrease
 
-                # Remove hit egg
-                allEggs.pop(eggHit)
+                    # Are we still alive?
+                    if health < 0:
+                        print("GAME OVER!!!")
+                        main_menu()
 
-            screen.blit(weapon.cannonImage, weapon.cannonRect)
+                    # We're Still alive, so remove the egg from the active list
+                    allEggs.remove(myEgg)
+                    continue
 
-        clock.tick(FPS)
-        pygame.display.update()
+            # Check all weeapons
+            for weapon in allWeapons:
+                if (activateFirstWeapon or newWeapon) and not weapon.isActive:
+                    #print("Activating weapon")
+                    weapon.isActive = True
+                    activateFirstWeapon = False
+
+                    # If this weapon was bought, subtract from cash
+                    if newWeapon:
+                        #cash -= weaponCost
+                        newWeapon = False
+
+                    
+                #if (hasUpgraded) and not weapon.isActive:
+                if hasUpgraded and weapon.isActive:
+                    weapon.ammoSpeed += 1
+
+                    #if hasUpgraded:
+                    #    cash -= upgradeCost             
+                    #    hasUpgraded = False
+
+                if not weapon.isActive:
+                    continue
+
+                '''for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_ESCAPE:
+                            main_menu()
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        if event.button == 1:
+                            clickBuy = True
+                            clickUpg = True
+
+                if button4.collidepoint(mx, my):
+                    if clickUpg:
+                        weapon.ammoSpeed += 1
+                        hasUpgraded = True
+                        clickUpg = False
+                        if cash < 0:
+                            cantUpg = True
+                if cantUpg == True:
+                    hasUpgraded = False
+
+                if button3.collidepoint(mx, my):
+                    if clickBuy:
+                        newWeapon = True        
+                        clickBuy = False       
+
+                click = False'''
+
+                # Move ammo
+                weapon.moveAmmo()
+
+                # Check if ammo hit an egg
+                eggHit = weapon.checkHit(allEggs)
+
+                if eggHit == -1:
+                    screen.blit(weapon.ammoImage, weapon.ammoRect)
+                else:
+                    #print("Blit boom")
+                    weapon.boomRect.center = allEggs[eggHit].eggRect.center
+                    screen.blit(weapon.boomImage, weapon.boomRect)
+                    cash += cashIncrement
+
+                    # Remove hit egg
+                    allEggs.pop(eggHit)
+
+                screen.blit(weapon.cannonImage, weapon.cannonRect)
+            
+            if hasUpgraded:
+                hasUpgraded = False
+
+            clock.tick(FPS)
+            pygame.display.update()
 
 
 # Display the main menu        
